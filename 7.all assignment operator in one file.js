5a29d33d3db4f1a91d8bdaaa00a08e4bd4c2610f
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-12-26 02:08:16 +0530

    new code

---
+++ 7.all assignment operator in one file.js
@@ -0,0 +1,28 @@
+var a=20;
+var b=a;
+console.log(b)
+
+var a=20;
+a+=20;
+var b=a;
+console.log(b)
+
+var a=20;
+a-=10;
+var b=a;
+console.log(b)
+
+var a=20;
+a*=10;
+var b=a;
+console.log(b)
+
+var a=20;
+a/=4;
+var b=a;
+console.log(b)
+
+var a=20;
+a%=10;
+var b=a;
+console.log(b)
